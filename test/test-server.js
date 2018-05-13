0ccb903ed14190b472ef2bd256984fd1103b6af5
Vincent Simone <vsimone@protonmail.com>
2018-05-13 00:13:26 -0700

    Added chai-http; added tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,11 @@
 const chai = require('chai');
-//const chaiHttp = require('chai-http');
+const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
-//chai.use(chaiHttp);
+chai.use(chaiHttp);
 
 describe('Blog Post', function() {
 
@@ -17,8 +17,77 @@ describe('Blog Post', function() {
     return closeServer();
   });
 
-  it('should just chill for now', function() {
-    expect(true).to.be.true;
+  it('should return list of blog posts on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+
+        expect(res.body.length).to.be.at.least(1);
+
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(post) {
+          expect(post).to.be.a('object');
+          expect(post).to.include.keys(expectedKeys);
+        });
+      });
+    });
+
+    it('should add a new blog post on POST', function() {
+    const newPost = {
+      'title': 'Cool Post, Bro',
+      'content': 'The sickest verbage you ever seen',
+      'author': 'Dave',
+      'publishDate': '5/13/18'
+    }
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newPost, {id: res.body.id}));
+      });
   });
+
+  it('should update an existing post on PUT', function() {
+
+    const updatedPost = {
+      'title': 'Cooler Post, Bro',
+      'content': 'You thought THAT was cool? Think again',
+      'author': 'Dave',
+      'publishDate': '5/13/18'
+    }
+
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        updatedPost.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/blog-posts/${updatedPost.id}`)
+          .send(updatedPost);
+      })
+      .then(function(res) {
+        //can't retrieve actual data because I don't know the ID's
+        expect(res).to.have.status(204);
+      });
+  });
+
+  it('should delete a blog post on DELETE', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+        .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  })
 
 });
